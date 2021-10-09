6a575cd32f6a938c5d48c9b4683bdae1d2ae8095
Alexandre Vila <alexandrevn@alu.ufc.br>
2021-10-09 18:21:54 -0300

    Component menu

---
+++ src/components/menu/index.jsx
@@ -0,0 +1,28 @@
+import './styles.css'
+
+import { Component } from "react";
+
+export class Menu extends Component {
+
+  
+  render() {
+    const { logado } = this.state;
+    
+    return (
+      
+      <img src=""/>
+
+      // {!logado && (
+      //   <nav className="nav">
+      //   <ul className="ul">
+      //     <li className="li"><a href="#">Menu</a></li>
+      //     <li className="li"><a href="#">Chapas</a></li>
+      //     <li className="li"><a href="#">Votar</a></li>
+      //   </ul>
+      // </nav>
+
+      // )}
+
+    );
+  }
+}
