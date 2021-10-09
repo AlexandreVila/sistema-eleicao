1057351e53d0f3d369cd8172d81a120b16667a49
Alexandre Vila <alexandrevn@alu.ufc.br>
2021-10-09 18:22:40 -0300

    Component footer

---
+++ src/components/footer/index.jsx
@@ -0,0 +1,13 @@
+import './styles.css'
+
+import { Component } from "react";
+
+export class Footer extends Component {
+  render() {
+    return (
+      <div className="footer">
+        <p className="p"> Criado por: Alexandre Vila Nova e Khennedy Bacule </p>
+      </div>
+    );
+  }
+}
