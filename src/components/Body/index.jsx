dbaadb5ae967b76d2fb8d23f07013673b8c9d58b
Alexandre Vila <alexandrevn@alu.ufc.br>
2021-10-09 22:15:47 -0300

    Atualização no componente body

--- src/components/Body/index.jsx
+++ src/components/Body/index.jsx
@@ -2,16 +2,24 @@ import './styles.css'
 
 import { TextInputs } from '../TextInputs'
 import { TituloLabel } from '../TituloLabel'
+import { Button } from '../Button';
 
-export const Body = ({  }) => {
+export const Body = () => {
 
   return (
-    <div className="body">
-      <TituloLabel value="Email: " />
-      <TextInputs placeholder="Digite seu email" type="text" />
-      <TituloLabel value="Senha: " />
-      <TextInputs placeholder="Digite sua senha" type="password" />
-    </div>
+    <section className="body">
+        
+        {/* Acrescentar logo do CA*/}
+
+        <TituloLabel value="Email: " />
+        <TextInputs placeholder="Digite seu email" type="text" />
+        <TituloLabel value="Senha: " />
+        <TextInputs placeholder="Digite sua senha" type="password" />
+      
+      
+        <Button placeholder="Entrar" />
+      
+    </section >
   );
 }
 
