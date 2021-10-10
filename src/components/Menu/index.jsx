b06d69caee1db66ea78c73ef78811c8a8bbea1f1
Alexandre Vila <alexandrevn@alu.ufc.br>
2021-10-09 22:16:45 -0300

    Adição do logout no component do menu

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -3,17 +3,16 @@ import './styles.css'
 export const Menu = () => {
 
   return (
-    <h1>Oi</h1>
-    // {!logado && (
-    //   <nav className="nav">
-    //   <ul className="ul">
-    //     <li className="li"><a href="#">Menu</a></li>
-    //     <li className="li"><a href="#">Chapas</a></li>
-    //     <li className="li"><a href="#">Votar</a></li>
-    //   </ul>
-    // </nav>
+    <nav className="nav">
+      <ul className="ul">
+        <li className="li"><a href="#">Menu</a></li>
+        <li className="li"><a href="#">Chapas</a></li>
+        <li className="li"><a href="#">Votar</a></li>
+        <li className="lastLi"><a href="#">Logout</a></li>
+      </ul>
+    </nav>
 
-    // )}
+
   );
 }
 
